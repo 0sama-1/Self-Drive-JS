9136c4b3b8a2b8cc059f430bc30ccc06a867e019
Osama <z5338349@ad.unsw.edu.au>
2025-01-05 14:26:36 +1100

    Roads have been designed, top view implemented, and borders have been added

--- main.js
+++ main.js
@@ -2,18 +2,25 @@
 const canvas = document.getElementById('myCanvas');
 canvas.width = 400;
 
+// Used to draw in the canvas:
+const ctx = canvas.getContext('2d');
+const road = new Road(canvas.width/2, canvas.width*0.9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
+
 const animate = () => {
   car.update();
   canvas.height = window.innerHeight;
+  
+  ctx.save();
+  ctx.translate(0, -car.y + canvas.height*0.9);
+
+  road.draw(ctx);
   car.draw(ctx);
+
+  ctx.restore();
   requestAnimationFrame(animate);
 }
 
-// Used to draw in the canvas:
-const ctx = canvas.getContext('2d');
-const car = new Car(200, 100, 30, 50);
-
-car.draw(ctx);
 
 animate();
 
